5c4883149a9456df2138cd4f9077a27de0d86a2b
Michael Slevin <michaelcslevin@gmail.com>
2017-10-04 17:04:04 +0100

    adds albums to console

--- db/console.rb
+++ db/console.rb
@@ -2,6 +2,10 @@ require("pry")
 require_relative("../models/albums")
 require_relative("../models/artists")
 
-artist1 = Artist.new({ name => "Sturm Cafe"})
+artist1 = Artist.new({ name => "CSS"})
 artist2 = Artist.new({ name => "Rammstein"})
 artist3 = Artist.new({ name => "KMFDM"})
+
+album1 = Album.new({ title => "Donkey", genre = "Electropop "})
+album2 = Album.new({ title => "Rosenrot", genre = "Metal"})
+album3 = Album.new({ title => "Hell Yeah", genre = "EBM"})
---
+++ models/albums.rb
@@ -0,0 +1,10 @@
+class Album
+
+  attr_accessor :title :genre
+  attr_reader :id
+
+  def initialize (album_hash)
+    @id = album_hash['id'].to_i if album_hash['id']
+    @title = album_hash['album_name']
+    @genre = album_hash['album_genre']
+  end
